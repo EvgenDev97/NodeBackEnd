da42fed41c3a93620b609f37be36e76082cfec3c
unknown <evgendev2022@gmail.com>
2025-03-03 10:36:21 +0300

    created user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,10 @@
 import db from "../db/db.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
+import {validRoles} from "../roles/roles.js";
 export async function registerController(req, res) {
-
     try{
-        const {username, password} = req.body;
+        const {username, password, role} = req.body;
         if(!username || !password){
             return res.status(400).json({error: "Username and password are required"});
         }
@@ -17,6 +17,11 @@ export async function registerController(req, res) {
         if (!passwordRegExp.test(password)) {
             return res.status(400).json({ message: "Password must be at least 8 characters long and contain at least 1 number, 1 lowercase letter, 1 uppercase letter, and 1 special character." });
         }
+
+        if(!validRoles.includes(role)){
+            return res.status(400).json({ message: "Invalid role" });
+        }
+
         const hashPassword = await bcrypt.hash(password, 12);
         const sqlRequest = `INSERT INTO users (username, password)
                                     VALUES (\$1, \$2)
@@ -34,6 +39,27 @@ export async function registerController(req, res) {
         });
     }catch (err){
         console.log(err);
-        res.status(500).json({message:"Catch has worked"});
+        res.status(500).json({message:"SERVER_ERROR"});
+    }
+}
+
+export async function authorizationController(req,res){
+    try{
+        const {username, password} = req.body;
+        if(!username || !password){
+            return res.status(400).json({error: "Username and password are required"});
+        }
+
+        const result = await db.query(
+            "SELECT * FROM users WHERE username = \$1 ", [username])
+        const user = result.rows[0];
+        if(user && (await bcrypt.compare(password, user.password))){
+            const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+            res.json({token: token});
+        }else{
+            res.status(401).json({message:"Invalid credentials"});
+        }
+    }catch (err){
+        res.status(500).json({message:"SERVER ERROR"});
     }
 }
