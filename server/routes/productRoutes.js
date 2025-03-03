28016db229635b4fa4c289d42dc13536b76f5e72
unknown <evgendev2022@gmail.com>
2025-03-03 10:39:19 +0300

    used auth middleware for router

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import {getProducts, createProduct, getProductById, updateProduct, deleteProduct} from "../controllers/productController.js";
+import {authenticateJWT} from "../middlewares/authenticateJWT.js";
 
 
 const router = express.Router();
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-router.post("/", createProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.post("/", authenticateJWT, createProduct);
+router.put("/:id", authenticateJWT, updateProduct);
+router.delete("/:id", authenticateJWT, deleteProduct);
 
 
 export default router;
