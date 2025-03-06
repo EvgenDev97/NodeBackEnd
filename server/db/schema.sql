c7e44892c88c427b8b0c05fd3ca289ea86d3e648
EvgenDevProfile <evgendev2022@gmail.com>
2025-03-06 11:39:42 +0300

    created user_roles references with users and roles

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,18 +1,41 @@
-CREATE TABLE IF NOT EXISTS products  (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR (255) NOT NULL,
-	image VARCHAR (255) NOT NULL,
-	price DECIMAL (10, 2) NOT NULL,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- Создание таблицы products
+CREATE TABLE IF NOT EXISTS products
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    image      VARCHAR(255) NOT NULL,
+    price      DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL UNIQUE
+-- Создание таблицы roles
+CREATE TABLE IF NOT EXISTS roles
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+    );
 
-)
+-- Вставка данных в таблицу roles
+INSERT INTO roles (name)
+VALUES ('admin'),
+       ('user')
+    ON CONFLICT (name) DO NOTHING;
 
 
-ALTER TABLE users ADD ROLE VARCHAR(10) DEFAULT "user";
+-- Создание таблицы users
+CREATE TABLE IF NOT EXISTS users
+(
+    id       SERIAL PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role     VARCHAR (255) NOT NULL
+    );
 
+-- Создание таблицы user_roles
+CREATE TABLE IF NOT EXISTS user_roles
+(
+    user_id INT REFERENCES users (id) ON DELETE CASCADE,
+    role_id INT REFERENCES roles (id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, role_id),
+    UNIQUE (user_id, role_id)  -- Уникальное ограничение на сочетание user_id и role_id
+    );
